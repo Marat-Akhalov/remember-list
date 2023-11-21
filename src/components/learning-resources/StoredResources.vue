5b5cf0e16f9cf7e859b999f28b07cf28724f0d54
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 16:47:03 +0400

    feat: add scoped style for ul

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -20,3 +20,11 @@ import LearningResources from './LearningResource.vue';
     props: ['resources'],
   }
 </script>
+
+<style scoped>
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+</style>
