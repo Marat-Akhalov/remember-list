0397609f5c4c5423dc5923474cbf8e5ce470247d
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 17:57:33 +0400

    feat: add ui card component

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    max-width: 40rem;
+    margin: 2rem auto;
+    padding: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  }
+</style>
