b6074a2b4457349777e4ed550b84a03a08cd0bd4
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 16:36:28 +0400

    feat: add store component

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <learning-resources
+    v-for="res in resources"
+    :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
+
+  export default {
+    components: {
+      LearningResources,
+    },
+    props: ['resources'],
+  }
+</script>
