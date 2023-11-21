65ebdcfe1d88ccd6edb62af14cbf31f5566b9ad6
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 15:14:17 +0400

    feat: add more data inside App file

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,14 @@
             title: 'Official guide',
             description: 'The official Vue.js docs',
             link: 'https://vuejs.org',
-          }
+          },
+          {
+            id: 'google',
+            title: 'Google',
+            description: 'Learn to google...',
+            link: 'https://google.com',
+          },
+
         ]
       }
     }
