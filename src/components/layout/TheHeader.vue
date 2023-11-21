14fc17ed0649ca75fa966aceba988c8b123f150a
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 18:13:31 +0400

    feat: add styles to the header component

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -9,3 +9,20 @@
     props: ['title'],
   }
 </script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+    border-radius: 5px;
+    background-color: #ED6A5E;
+  }
+
+  header h1 {
+    margin: 0;
+    color: #fff;
+  }
+</style>
