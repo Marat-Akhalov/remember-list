03948ec1d7f3a206e8f3bd0e33208cfe96cea6cb
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-21 15:12:25 +0400

    feat: add entry App file with dummy data inside

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        storedResources: [
+          {
+            id: 'official-guide',
+            title: 'Official guide',
+            description: 'The official Vue.js docs',
+            link: 'https://vuejs.org',
+          }
+        ]
+      }
+    }
+  }
+</script>
