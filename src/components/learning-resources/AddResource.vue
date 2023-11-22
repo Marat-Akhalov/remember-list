e816d031e464b2c73d9b3a34cc7d59a8bc855685
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-22 13:19:59 +0400

    feat: add form with basic styles

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,49 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text">
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url">
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
+
+<style scoped>
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-weight: bold;
+  }
+
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+    font: inherit;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #ed6a5e;
+    outline: none;
+    background-color: rgba(14, 169, 180, 0.2);
+  }
+
+  .form-control {
+    margin: 1rem 0;
+  }
+</style>
