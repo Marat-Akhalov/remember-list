25a8effc6195e30c1ce60ec65a9818b391c2fa35
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-22 09:52:32 +0400

    feat: add style to the btn component

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -1,3 +1,19 @@
 <template>
   <button></button>
 </template>
+
+<style scoped>
+  button {
+    padding: 75rem 1.5rem;
+    border: 1px solid salmon;
+    color: whitesmoke;
+    background-color: #045257;
+    cursor: pointer;
+  }
+
+  button:hover,
+  button:active {
+    border-color: blue;
+    background-color: #0ea9b4;
+  }
+</style>
