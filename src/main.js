e0d6d1b375fb779430e04b815b340673665a8ce2
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-22 14:24:53 +0400

    feat: add base dialog to global file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
 
+app.component('base-dialog', BaseDialog);
 app.component('base-button', BaseButton);
 app.component('base-card', BaseCard);
 
