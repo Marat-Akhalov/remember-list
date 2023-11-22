e4d6c8859861fcce45e580ba687df558a7ae4813
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-22 11:17:31 +0400

    feat: add components to dinamically render proper tags

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-recourse')">Add Recourses</base-button>
+  </base-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selectedTab: 'stored-recourses',
+      }
+    },
+    methods: {
+      setSelectedTab(tab) {
+        this.selectedTab = tab;
+      }
+    }
+  }
+</script>
