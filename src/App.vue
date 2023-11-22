e32c0b9b30d4388a80bdfcbb349e12f27aa31a84
Marat Zaynulabidov <maratakhalov14@gmail.com>
2023-11-22 11:28:19 +0400

    fix: remove data and place component instead

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <the-header title="Remember Me"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
   import TheHeader from './components/layout/TheHeader.vue';
+  import TheResources from './components/learning-resources/TheResources.vue';
 
   export default {
     components: {
       TheHeader,
+      TheResources,
     },
-    data() {
-      return {
-        storedResources: [
-          {
-            id: 'official-guide',
-            title: 'Official guide',
-            description: 'The official Vue.js docs',
-            link: 'https://vuejs.org',
-          },
-          {
-            id: 'google',
-            title: 'Google',
-            description: 'Learn to google...',
-            link: 'https://google.com',
-          },
-        ]
-      }
-    }
   }
 </script>
 
